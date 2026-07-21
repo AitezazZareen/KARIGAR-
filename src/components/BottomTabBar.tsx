9bf9a525471ccef9cf999453e87fc701661ec517
AitezazZareen <aitezazzareen@gmail.com>
2026-07-22 02:41:25 +0500

    Replace deprecated shadow* style props with boxShadow

--- src/components/BottomTabBar.tsx
+++ src/components/BottomTabBar.tsx
@@ -83,10 +83,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: -24,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.2,
-    shadowRadius: 6,
+    boxShadow: '0px 4px 6px rgba(0,0,0,0.2)',
     elevation: 4,
   },
 });
